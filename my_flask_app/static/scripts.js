723c24db83a5bac0c50b6880e49135585e8f2b75
Jin <jinh12@gmail.com>
2023-12-10 23:00:27 +0900

    flask: make note move auto

--- my_flask_app/static/scripts.js
+++ my_flask_app/static/scripts.js
@@ -12,6 +12,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Make the note draggable
         makeDraggable(newNote);
+
+        // Set initial position and start floating animation
+        setInitialPosition(newNote);
+        floatNote(newNote);
     });
 
     function makeDraggable(element) {
@@ -59,5 +63,50 @@ document.addEventListener('DOMContentLoaded', function () {
             document.onmouseup = null;
             document.onmousemove = null;
         }
+    }
+
+    function setInitialPosition(element) {
+        const containerRect = notesContainer.getBoundingClientRect();
+        const elementRect = element.getBoundingClientRect();
+
+        const maxX = containerRect.width - elementRect.width;
+        const maxY = containerRect.height - elementRect.height;
+
+        const initialX = Math.random() * maxX;
+        const initialY = Math.random() * maxY;
+
+        element.style.transition = 'transform 0s'; // Disable transition for initial position
+        element.style.transform = `translate(${initialX}px, ${initialY}px)`;
+    }
+
+
+    function floatNote(element) {
+        const containerRect = notesContainer.getBoundingClientRect();
+        const elementRect = element.getBoundingClientRect();
+
+        const maxX = containerRect.width - elementRect.width;
+        const maxY = containerRect.height - elementRect.height;
+
+        const animationSpeed = 2; // Adjust the speed as needed
+
+        function animate() {
+            let newX = Math.random() * maxX;
+            let newY = Math.random() * maxY;
+
+            element.style.transition = `transform ${animationSpeed}s ease-in-out`;
+            element.style.transform = `translate(${newX}px, ${newY}px)`;
+
+            setTimeout(() => {
+                // Reset the position after the animation
+                element.style.transition = 'none';
+                element.style.transform = 'none';
+
+                // Repeat the animation after a delay
+                setTimeout(animate, 1000);
+            }, animationSpeed * 1000);
+        }
+
+        // Start the initial animation after a short delay to allow setting the initial position
+        setTimeout(animate, 100);
     }
 });
